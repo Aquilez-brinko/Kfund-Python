be914f41b20107fcb5b05982d85b662a3f5679f9
Aquilez-brinko <52606689+Aquilez-brinko@users.noreply.github.com>
2022-01-22 21:07:35 +0200

    les6

---
+++ 6classwork.py
@@ -0,0 +1,20 @@
+for i in range(1,10):
+    print(i, end=" ")
+print("Конец\n")
+
+for i in range(1,10,2):
+    print(i, end=" ")
+print("Конец\n")
+
+for i in range(10,0,-2):
+    print(i, end=" ")
+print("Конец\n")
+
+for i in range(10):
+    print(i, end=" ")
+print("Конец\n")
+
+
+for i in range(1,10):
+    print(i**2, end=" ")
+print("Конец\n")
