86967dbd267ff14808a1bb0af67fdd45fbeda882
Aquilez-brinko <52606689+Aquilez-brinko@users.noreply.github.com>
2022-01-29 17:59:48 +0200

    les9

---
+++ 9homework.py
@@ -0,0 +1,11 @@
+pizza = ['Carbonara', 'Neopolitan', 'Margarita']
+for i in pizza:
+    print(f'I like {i} pizza')
+print(f'I really love pizza!\n')
+
+animals = ['Lion', 'Tiger', 'Cheetah']
+for i in animals:
+    print(f'A {i} would make a great predator')
+print('Any of these animals would make a great predator')
+
+
