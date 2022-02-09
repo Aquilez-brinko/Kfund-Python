b323d89a898dfb55de4cb6ebc3342f40ddfe653b
Aquilez-brinko <52606689+Aquilez-brinko@users.noreply.github.com>
2022-02-09 02:23:57 +0200

    les11

---
+++ 11classwork.py
@@ -0,0 +1,28 @@
+user = {
+     "u1": "Человек",
+     "u2": "Супергерой"
+}
+new_user = user["u2"]
+message = f"Ты прошел игру, теперь ты {new_user}"
+print(message)
+print(user)
+user["u3"] = "Волк"
+user["u4"] = "Лиса"
+print(user)
+
+
+# list1 = [[65456, 456456, 465465], 'gdfgdfdgfdgf', 'gfdgdfdgfgfd', ['gdfdgfdgf', 'fgdfggdfgdf']]
+# for i in list1:
+#     if type(i) == list:
+#         for a in i:
+#             print(a)
+#     else:
+#         print(i)
+#user = {"u1": "human",
+#        "u2": "superhero"
+#        }
+#new_user = user["u2"]
+#print(user)
+#print(f"Wow! You transformed to a {new_user}!")
+#user["u3"] = "Added user3"
+#print(user)
